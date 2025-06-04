5ea1455ee13e94f9a618c44c2b4d2c2c5d1be350
unsignedmind <niklas-schmidt@outlook.de>
2025-06-04 20:10:38 +0200

    feat: implement Angular 20 todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
